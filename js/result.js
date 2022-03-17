f2fc73c4efb5d7cf68a4996904d3afa7a8f25f42
加藤義隆 <991024yoshi@gmail.com>
2022-03-17 18:27:57 +0900

    add send data logic

--- js/result.js
+++ js/result.js
@@ -2,9 +2,22 @@
 
 window.onload = function(){
     var data = location.href.split("?")[1];
-    var text = data.split("=")[1];
-    document.getElementById("currentMRR").innerHTML = decodeURIComponent(text);
-    document.getElementById("currentARR").innerHTML = decodeURIComponent(text);
-    document.getElementById("MRR").innerHTML = decodeURIComponent(text);
-    document.getElementById("ARR").innerHTML = decodeURIComponent(text);
+    var client = Number(data.split("=")[1].replace(/[^0-9]/g, ''));
+    var cost = Number(data.split("=")[2].replace(/[^0-9]/g, ''));
+    var churn_rate = Number(data.split("=")[3]);
+
+    function getImprovedrate() {
+        if (0.1 <= churn_rate && churn_rate < 1) {
+            return (churn_rate - 0.1);
+        } else {
+            return (churn_rate - 1);
+        }
+    }
+
+    var improve_rate = getImprovedrate();
+    
+    document.getElementById("currentMRR").innerHTML = (client * cost * (churn_rate * 0.01));
+    document.getElementById("currentARR").innerHTML = (client * cost * (churn_rate * 0.01) * 12);
+    document.getElementById("MRR").innerHTML = (client * cost * (improve_rate * 0.01));
+    document.getElementById("ARR").innerHTML = (client * cost * (improve_rate * 0.01) * 12);
 }
