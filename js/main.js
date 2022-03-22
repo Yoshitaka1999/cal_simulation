5c0095bead5df7153d6afa57c1400f52e32db387
加藤義隆 <991024yoshi@gmail.com>
2022-03-22 12:11:34 +0900

    ロジック修正

--- js/main.js
+++ js/main.js
@@ -6,13 +6,15 @@ const form = document.getElementById("form");
 submit.addEventListener('click', (e) => {
     e.preventDefault();
 
+    var flag = 0;
+
     const cost = document.getElementById('cost');
     const errMsgCost = document.querySelector('.err-msg-cost');
     if(!cost.value.match(/(?!0+$)^[0-9]+$/)) {
         errMsgCost.classList.add('form-invalid');
         errMsgCost.textContent = '空欄もしくは入力に誤りがあります';
         cost.classList.add('input-invalid');
-        return;
+        flag++;
     } else {
         errMsgCost.textContent ='';
         cost.classList.remove('input-invalid');
@@ -24,7 +26,7 @@ submit.addEventListener('click', (e) => {
         errMsgClient.classList.add('form-invalid');
         errMsgClient.textContent = '空欄もしくは入力に誤りがあります';
         client.classList.add('input-invalid');
-        return;
+        flag++;
     } else {
         errMsgClient.textContent ='';
         client.classList.remove('input-invalid');
@@ -32,19 +34,20 @@ submit.addEventListener('click', (e) => {
 
     const churn_rate = document.getElementById('churn_rate');
     const errMsgRate = document.querySelector('.err-msg-churn-rate');
-    if(!churn_rate.value.match(/(?!0+$)^([1-9]\d*|0)(\.\d{1})?$/)) {
+    if(!churn_rate.value.match(/(?!0+$)^([1-9]\d*|0)(\.\d{1})?$/) || churn_rate.value.match(0.1)) {
         errMsgRate.classList.add('form-invalid');
         errMsgRate.textContent = '空欄もしくは入力に誤りがあります';
         churn_rate.classList.add('input-invalid');
-        return;
+        flag++;
     } else {
         errMsgRate.textContent ='';
         churn_rate.classList.remove('input-invalid');
     }
 
-    form.method = "GET";
-    form.action = "result.html";
-    form.submit();
+    if(flag == 0) {
+        form.action = "result.html";
+        form.submit();     
+      } else {
+        flag = 0;
+      }
 })
-
-    
--- js/result.js
+++ js/result.js
@@ -7,7 +7,7 @@ window.onload = function(){
     var churn_rate = Number(data.split("=")[3]);
 
     function getImprovedrate() {
-        if (0.1 <= churn_rate && churn_rate < 1) {
+        if (0.1 <= churn_rate && churn_rate <= 1) {
             return (churn_rate - 0.1);
         } else {
             return (churn_rate - 1);
@@ -16,8 +16,8 @@ window.onload = function(){
 
     var improve_rate = getImprovedrate();
     
-    document.querySelector(".currentMRR").innerHTML = (client * cost * (churn_rate * 0.01));
-    document.querySelector(".currentARR").innerHTML = (client * cost * (churn_rate * 0.01) * 12);
-    document.querySelector(".improvedMRR").innerHTML = (client * cost * (improve_rate * 0.01));
-    document.querySelector(".improvedARR").innerHTML = (client * cost * (improve_rate * 0.01) * 12);
+    document.querySelector(".currentMRR").innerHTML = (client * cost * (churn_rate * 0.01)).toLocaleString();
+    document.querySelector(".currentARR").innerHTML = (client * cost * (churn_rate * 0.01) * 12).toLocaleString();
+    document.querySelector(".improvedMRR").innerHTML = (client * cost * (improve_rate * 0.01)).toLocaleString();
+    document.querySelector(".improvedARR").innerHTML = (client * cost * (improve_rate * 0.01) * 12).toLocaleString();
 }
